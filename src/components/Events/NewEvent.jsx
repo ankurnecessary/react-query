71e1ac7e21d0a93d2bba1b9080e3ffd38ace506c
Ankur Gupta <ankurnecessary@gmail.com>
2024-06-12 10:11:35 +0930

    [react-query]: Used "useMutation()" hook of react-query to add a new event in the database.

--- src/components/Events/NewEvent.jsx
+++ src/components/Events/NewEvent.jsx
@@ -1,24 +1,38 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
 
-import Modal from '../UI/Modal.jsx';
-import EventForm from './EventForm.jsx';
+import Modal from "../UI/Modal.jsx";
+import EventForm from "./EventForm.jsx";
+import { useMutation } from "@tanstack/react-query";
+import { createNewEvent } from "../../util/http.js";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function NewEvent() {
+  const { mutate, isPending, isError, error } = useMutation({
+    mutationFn: createNewEvent,
+  });
   const navigate = useNavigate();
 
-  function handleSubmit(formData) {}
+  function handleSubmit(formData) {
+    mutate({ event: formData });
+  }
 
   return (
-    <Modal onClose={() => navigate('../')}>
+    <Modal onClose={() => navigate("../")}>
       <EventForm onSubmit={handleSubmit}>
-        <>
-          <Link to="../" className="button-text">
-            Cancel
-          </Link>
-          <button type="submit" className="button">
-            Create
-          </button>
-        </>
+        <div className="form-actions">
+          {isPending && "Submitting..."}
+          {!isPending && (
+            <>
+              <Link to="../" className="button-text">
+                Cancel
+              </Link>
+              <button type="submit" className="button">
+                Create
+              </button>
+            </>
+          )}
+        </div>
+        {isError && <ErrorBlock title="Error while saving an event" message={error.message} />}
       </EventForm>
     </Modal>
   );
